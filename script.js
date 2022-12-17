6cef5915e7e801484f1023fe1fb3b0c829cd242d
Emilie Rollandin <info@archistico.com>
2022-12-17 23:52:11 +0100

    aggiunta funziona converti metri

--- script.js
+++ script.js
@@ -19,6 +19,21 @@ function getDettagliCorpo(arr, corpo) {
     return { diametro: diametro, distanza_dal_sole: distanza_dal_sole }
 }
 
+function metriToUnita(metri) {
+    if(metri < 0.01) {
+        return Math.round(metri*1000*100)/100 + " mm"
+    }
+    if(metri < 1) {
+        return Math.round(metri*100*100)/100 + " cm"
+    }
+    if(metri < 1000) {
+        return Math.round(metri*100)/100 + " m"
+    }
+    if(metri > 1000) {
+        return Math.round(metri/1000*100)/100 + " km"
+    }
+}
+
 function calculate(diametroTerraNuovo) {
     const dimensioni_corpi_celesti = [
         { corpo: 'sole',             diametro: 1390950000,   distanza_dal_sole: 0 },
@@ -34,7 +49,7 @@ function calculate(diametroTerraNuovo) {
         { corpo: 'plutone',          diametro: 4376600,      distanza_dal_sole: 5906375000000 },
         { corpo: 'eris',             diametro: 2326000,      distanza_dal_sole: 10121073571000 },
         { corpo: 'eliosfera',        diametro: 0,            distanza_dal_sole: 13463809875000 },
-        { corpo: 'proxima centauri', diametro: 201550000,    distanza_dal_sole: 40176136500000000 }
+        { corpo: 'proxima_centauri', diametro: 201550000,    distanza_dal_sole: BigInt('40176136500000000') }
     ]
 
     // CALCOLO DIVISORE
@@ -56,19 +71,19 @@ function calculate(diametroTerraNuovo) {
     const diametro_eris = getDettagliCorpo(dimensioni_corpi_celesti,'eris').diametro / divisore
     const diametro_eliosfera = getDettagliCorpo(dimensioni_corpi_celesti,'eliosfera').diametro / divisore
 
-    const html_diametro_sole = (document.getElementById('diametro_sole')).innerHTML = diametro_sole.toFixed(3)
-    const html_diametro_mercurio = (document.getElementById('diametro_mercurio')).innerHTML = diametro_mercurio.toFixed(3)
-    const html_diametro_venere = (document.getElementById('diametro_venere')).innerHTML = diametro_venere.toFixed(3)
-    const html_diametro_terra = (document.getElementById('diametro_terra')).innerHTML = diametro_terra.toFixed(3)
-    const html_diametro_luna = (document.getElementById('diametro_luna')).innerHTML = diametro_luna.toFixed(3)
-    const html_diametro_marte = (document.getElementById('diametro_marte')).innerHTML = diametro_marte.toFixed(3)
-    const html_diametro_giove = (document.getElementById('diametro_giove')).innerHTML = diametro_giove.toFixed(3)
-    const html_diametro_saturno = (document.getElementById('diametro_saturno')).innerHTML = diametro_saturno.toFixed(3)
-    const html_diametro_urano = (document.getElementById('diametro_urano')).innerHTML = diametro_urano.toFixed(3)
-    const html_diametro_nettuno = (document.getElementById('diametro_nettuno')).innerHTML = diametro_nettuno.toFixed(3)
-    const html_diametro_plutone = (document.getElementById('diametro_plutone')).innerHTML = diametro_plutone.toFixed(3)
-    const html_diametro_eris = (document.getElementById('diametro_eris')).innerHTML = diametro_eris.toFixed(3)
-    const html_diametro_eliosfera = (document.getElementById('diametro_eliosfera')).innerHTML = diametro_eliosfera.toFixed(3)
+    const html_diametro_sole = (document.getElementById('diametro_sole')).innerHTML = metriToUnita(diametro_sole)
+    const html_diametro_mercurio = (document.getElementById('diametro_mercurio')).innerHTML = metriToUnita(diametro_mercurio)
+    const html_diametro_venere = (document.getElementById('diametro_venere')).innerHTML = metriToUnita(diametro_venere)
+    const html_diametro_terra = (document.getElementById('diametro_terra')).innerHTML = metriToUnita(diametro_terra)
+    const html_diametro_luna = (document.getElementById('diametro_luna')).innerHTML = metriToUnita(diametro_luna)
+    const html_diametro_marte = (document.getElementById('diametro_marte')).innerHTML = metriToUnita(diametro_marte)
+    const html_diametro_giove = (document.getElementById('diametro_giove')).innerHTML = metriToUnita(diametro_giove)
+    const html_diametro_saturno = (document.getElementById('diametro_saturno')).innerHTML = metriToUnita(diametro_saturno)
+    const html_diametro_urano = (document.getElementById('diametro_urano')).innerHTML = metriToUnita(diametro_urano)
+    const html_diametro_nettuno = (document.getElementById('diametro_nettuno')).innerHTML = metriToUnita(diametro_nettuno)
+    const html_diametro_plutone = (document.getElementById('diametro_plutone')).innerHTML = metriToUnita(diametro_plutone)
+    const html_diametro_eris = (document.getElementById('diametro_eris')).innerHTML = metriToUnita(diametro_eris)
+    const html_diametro_eliosfera = (document.getElementById('diametro_eliosfera')).innerHTML = metriToUnita(diametro_eliosfera)
 
 
     // CALCOLO DISTANZE
@@ -84,21 +99,21 @@ function calculate(diametroTerraNuovo) {
     const distanza_dal_sole_plutone = getDettagliCorpo(dimensioni_corpi_celesti,'plutone').distanza_dal_sole / divisore
     const distanza_dal_sole_eris = getDettagliCorpo(dimensioni_corpi_celesti,'eris').distanza_dal_sole / divisore
     const distanza_dal_sole_eliosfera = getDettagliCorpo(dimensioni_corpi_celesti,'eliosfera').distanza_dal_sole / divisore
-    const distanza_dal_sole_proxima_centauri = getDettagliCorpo(dimensioni_corpi_celesti,'proxima_centauri').distanza_dal_sole / divisore
+    const distanza_dal_sole_proxima_centauri = Number(BigInt(getDettagliCorpo(dimensioni_corpi_celesti,'proxima_centauri').distanza_dal_sole) / BigInt(divisore))
 
-    const html_distanza_dal_sole_mercurio = (document.getElementById('distanza_mercurio')).innerHTML = distanza_dal_sole_mercurio.toFixed(3)
-    const html_distanza_dal_sole_venere = (document.getElementById('distanza_venere')).innerHTML = distanza_dal_sole_venere.toFixed(3)
-    const html_distanza_dal_sole_terra = (document.getElementById('distanza_terra')).innerHTML = distanza_dal_sole_terra.toFixed(3)
-    const html_distanza_dal_sole_luna = (document.getElementById('distanza_luna')).innerHTML = distanza_dal_sole_luna.toFixed(3)
-    const html_distanza_dal_sole_marte = (document.getElementById('distanza_marte')).innerHTML = distanza_dal_sole_marte.toFixed(3)
-    const html_distanza_dal_sole_giove = (document.getElementById('distanza_giove')).innerHTML = distanza_dal_sole_giove.toFixed(3)
-    const html_distanza_dal_sole_saturno = (document.getElementById('distanza_saturno')).innerHTML = distanza_dal_sole_saturno.toFixed(3)
-    const html_distanza_dal_sole_urano = (document.getElementById('distanza_urano')).innerHTML = distanza_dal_sole_urano.toFixed(3)
-    const html_distanza_dal_sole_nettuno = (document.getElementById('distanza_nettuno')).innerHTML = distanza_dal_sole_nettuno.toFixed(3)
-    const html_distanza_dal_sole_plutone = (document.getElementById('distanza_plutone')).innerHTML = distanza_dal_sole_plutone.toFixed(3)
-    const html_distanza_dal_sole_eris = (document.getElementById('distanza_eris')).innerHTML = distanza_dal_sole_eris.toFixed(3)
-    const html_distanza_dal_sole_eliosfera = (document.getElementById('distanza_eliosfera')).innerHTML = distanza_dal_sole_eliosfera.toFixed(3)
-    const html_distanza_dal_sole_proxima_centauri = (document.getElementById('distanza_proxima_centauri')).innerHTML = distanza_dal_sole_proxima_centauri.toFixed(3)
+    const html_distanza_dal_sole_mercurio = (document.getElementById('distanza_mercurio')).innerHTML = metriToUnita(distanza_dal_sole_mercurio)
+    const html_distanza_dal_sole_venere = (document.getElementById('distanza_venere')).innerHTML = metriToUnita(distanza_dal_sole_venere)
+    const html_distanza_dal_sole_terra = (document.getElementById('distanza_terra')).innerHTML = metriToUnita(distanza_dal_sole_terra)
+    const html_distanza_dal_sole_luna = (document.getElementById('distanza_luna')).innerHTML = metriToUnita(distanza_dal_sole_luna)
+    const html_distanza_dal_sole_marte = (document.getElementById('distanza_marte')).innerHTML = metriToUnita(distanza_dal_sole_marte)
+    const html_distanza_dal_sole_giove = (document.getElementById('distanza_giove')).innerHTML = metriToUnita(distanza_dal_sole_giove)
+    const html_distanza_dal_sole_saturno = (document.getElementById('distanza_saturno')).innerHTML = metriToUnita(distanza_dal_sole_saturno)
+    const html_distanza_dal_sole_urano = (document.getElementById('distanza_urano')).innerHTML = metriToUnita(distanza_dal_sole_urano)
+    const html_distanza_dal_sole_nettuno = (document.getElementById('distanza_nettuno')).innerHTML = metriToUnita(distanza_dal_sole_nettuno)
+    const html_distanza_dal_sole_plutone = (document.getElementById('distanza_plutone')).innerHTML = metriToUnita(distanza_dal_sole_plutone)
+    const html_distanza_dal_sole_eris = (document.getElementById('distanza_eris')).innerHTML = metriToUnita(distanza_dal_sole_eris)
+    const html_distanza_dal_sole_eliosfera = (document.getElementById('distanza_eliosfera')).innerHTML = metriToUnita(distanza_dal_sole_eliosfera)
+    const html_distanza_dal_sole_proxima_centauri = (document.getElementById('distanza_proxima_centauri')).innerHTML = metriToUnita(distanza_dal_sole_proxima_centauri)
 }
 
 onChangeValue()
